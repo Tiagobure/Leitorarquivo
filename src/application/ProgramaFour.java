c8a8860affb0cbe62c7c4513ec6b52f20e1eef12
Tiago santos palma <tiagosantos.mr@gmail.com>
2025-02-10 12:43:05 -0300

    Fix: manioulando pastas com file

---
+++ src/application/ProgramaFour.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class ProgramaFour {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter a foder patch: ");
+		String strPath = sc.nextLine();
+		
+		File patch = new File(strPath);
+		
+		File[] folders = patch.listFiles(File::isDirectory);
+		System.out.println("FOLDERS: ");
+		for(File folder : folders) {
+			System.out.println(folder);
+		}
+		
+		File[] files = patch.listFiles(File::isFile);
+		System.out.println("FILES: ");
+		for(File file : files) {
+			System.out.println(file);
+		}
+		boolean success = new File(strPath +"\\subdir").mkdir();
+		System.out.println("Directory created successfully: " + success);
+		
+		sc.close();
+		
+	}
+
+}
